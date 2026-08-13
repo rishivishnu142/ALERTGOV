0e57740f7067e3fe2012bb23457fb37bc0c8a731
Rishi Vishnu <rv75749d@gmail.com>
2026-08-13 13:35:18 +0530

    Update API Documentation PDF with latest microservice endpoints

--- generate_pdf.js
+++ generate_pdf.js
@@ -1,7 +1,7 @@
 var markdownpdf = require("markdown-pdf")
   , fs = require("fs")
 
-fs.createReadStream("C:/Users/RISHIKESH V/.gemini/antigravity-ide/brain/85aab00d-c35c-40c9-a9ff-e3e1904ae1bd/api_documentation.md")
+fs.createReadStream("api_documentation.md")
   .pipe(markdownpdf({
     paperFormat: 'A4',
     cssPath: 'css.css'
